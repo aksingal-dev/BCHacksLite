0e1749561d91ab25aa7701636c11e713704ca1b9
ItsMyFuneral <71358703+ItsMyFuneral@users.noreply.github.com>
2020-10-31 15:24:18 -0700

    added candy support

--- StoryNode.java
+++ StoryNode.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.io.*;
 
 public class StoryNode {
 	private String output; 
@@ -13,59 +12,64 @@ public class StoryNode {
 	private String optD;
 	
 	private boolean[] skillChecks = new boolean[4];
+	private Candy[] candiesGiven = new Candy[4];
 	private int ending = -1;
 	
+<<<<<<< Updated upstream
 	public StoryNode trigger(Player p) throws IOException {
 	//	for(int i = 0; i < 50; i++)							// clear the console
 	//		System.out.println();
+=======
+	public StoryNode trigger(Player p){
+		for(int i = 0; i < 50; i++)							// clear the console
+			System.out.println();
+>>>>>>> Stashed changes
 		
 		System.out.println(this.output());
 		if(ending != -1)
 		{
-			return new StoryNode(Integer.toString(ending),null,null,null,null);
+			return new StoryNode(Integer.toString(ending));
 		}
-		if(b == null && c == null && d == null)
+		String s = "";
+		Scanner sc = new Scanner(System.in);
+		boolean valid;
+		do {
+			System.out.println("a: " + optA);
+			System.out.println("b: " + optB);
+			if(c != null)
+			{
+				System.out.println("c: " + optC);
+				if(d != null)
+					System.out.println("d: " + optD);
+			}
+			s = sc.next().toLowerCase();
+			valid = !((!s.equals("a") && !s.equals("b") && !s.equals("c") && !s.equals("d")) ||
+					  (s.equals("c") && c == null) ||
+					  (s.equals("d") && d == null));
+			if(!valid)
+				System.out.println("I'm sorry, I didn't understand that.");
+		} while(!valid);
+		sc.close();
+		
+		if(s.equals("a"))
 		{
-			System.out.println("\nPress enter to continue.");		// only one option i.e. option A
-			
-			// Have player press the enter key to continue.
-			// ISSUE: Currently throws an IOException: Stream closed.
-			// Using a Scanner throws an exception about not having a next line.
-			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-			input.readLine();
-			input.close();
+			p.addCandy(candiesGiven[0]);
 			return a;
 		}
-		else {
-			String s = "";
-			Scanner sc = new Scanner(System.in);
-			boolean valid;
-			do {
-				System.out.println("a: " + optA);
-				System.out.println("b: " + optB);
-				if(c != null)
-				{
-					System.out.println("c: " + optC);
-					if(d != null)
-						System.out.println("d: " + optD);
-				}
-				s = sc.next().toLowerCase();
-				valid = !((!s.equals("a") && !s.equals("b") && !s.equals("c") && !s.equals("d")) ||
-						  (s.equals("c") && c == null) ||
-						  (s.equals("d") && d == null));
-				if(!valid)
-					System.out.println("I'm sorry, I didn't understand that.");
-			} while(!valid);
-			sc.close();
-			
-			if(s.equals("a"))
-				return a;
-			if(s.equals("b"))
-				return b;
-			if(s.equals("c"))
-				return c;
-			if(s.equals("d"))
-				return d;
+		if(s.equals("b"))
+		{
+			p.addCandy(candiesGiven[1]);
+			return b;
+		}
+		if(s.equals("c"))
+		{
+			p.addCandy(candiesGiven[2]);
+			return c;
+		}
+		if(s.equals("d"))
+		{
+			p.addCandy(candiesGiven[3]);
+			return d;
 		}
 		
 		return null;							// this can never happen, but Eclipse thinks we could get here, so...
@@ -100,6 +104,11 @@ public class StoryNode {
 		this.d = null;
 	}
 	
+	public void setOutput(String out) { output = out; }
+	public void setA(StoryNode n) { a = n; }
+	public void setB(StoryNode n) { b = n; }
+	public void setC(StoryNode n) { c = n; }
+	public void setD(StoryNode n) { d = n; }
 	public void setA(StoryNode n, String opt) { a = n; optA = opt;}
 	public void setB(StoryNode n, String opt) { b = n; optB = opt;}
 	public void setC(StoryNode n, String opt) { c = n; optC = opt;}
