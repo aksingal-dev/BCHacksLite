92d49184a57e52e9acfa19ddf84a61d11f727861
ItsMyFuneral <71358703+ItsMyFuneral@users.noreply.github.com>
2020-10-31 21:22:38 -0700

    Removed GUI code

--- StoryNode.java
+++ StoryNode.java
@@ -1,12 +1,9 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.Random;
+
 import java.util.*;
 
-public class StoryNode
-{
+public class StoryNode {
 	private String output;
+	private StoryNode next;										// intended to be for consecutive nodes
 	private StoryNode a;
 	private StoryNode b;
 	private StoryNode c;
@@ -16,25 +13,36 @@ public class StoryNode
 	private String optC;
 	private String optD;
 	
-	private int[] skillChecks = new int[4];		//-1 for none, 0 for cha, 1 for dex, 2 for str
-	private Candy[] candiesGiven = new Candy[4];
-	private int ending = -1;  
+	private Candy toGive = null;
+	private Candy[] candiesGiven = {null, null, null, null};
+	private int ending = -1;
 	
-	public StoryNode trigger(Player p)
-   {
-		System.out.println(this.output());
+	@SuppressWarnings("resource")
+	public StoryNode trigger(Player p){
+		if(this.output().contains("startled by a mummy"))
+			this.setOutput(this.output() + p.dropCandy());
+		if(!this.output().equals(""))
+			System.out.println("\n----------\n");
+		System.out.println(this.output()+"\n");
+		if(toGive != null)
+		{
+			System.out.println("\n");
+			p.addCandy(toGive);
+			System.out.println("\n");
+		}
 		if(ending != -1)
 		{
 			return new StoryNode(Integer.toString(ending));
 		}
-		if(b == null && c == null && d == null)					// straight-through node
+		if(next != null)
 		{
-			p.addCandy(candiesGiven[0]);
-			return a;
+			if(candiesGiven[0] != null)
+				next.setCandy(candiesGiven[0]);
+			return next;
 		}
 		String s = "";
-		Scanner sc = new Scanner(System.in);
 		boolean valid;
+		Scanner sc = new Scanner(System.in);
 		do {
 			System.out.println("a: " + optA);
 			System.out.println("b: " + optB);
@@ -44,48 +52,39 @@ public class StoryNode
 				if(d != null)
 					System.out.println("d: " + optD);
 			}
-			s = sc.next().toLowerCase();
+			
+			s = sc.nextLine().toLowerCase();
+			
 			valid = !((!s.equals("a") && !s.equals("b") && !s.equals("c") && !s.equals("d")) ||
 					  (s.equals("c") && c == null) ||
 					  (s.equals("d") && d == null));
 			if(!valid)
 				System.out.println("I'm sorry, I didn't understand that.");
 		} while(!valid);
-		sc.close();
+		
 		
 		if(s.equals("a"))
 		{
-			p.addCandy(candiesGiven[0]);
+			a.setCandy(candiesGiven[0]);
 			return a;
 		}
 		if(s.equals("b"))
 		{
-			p.addCandy(candiesGiven[1]);
+			b.setCandy(candiesGiven[1]);
 			return b;
 		}
 		if(s.equals("c"))
 		{
-			p.addCandy(candiesGiven[2]);
+			c.setCandy(candiesGiven[2]);
 			return c;
 		}
 		if(s.equals("d"))
 		{
-			p.addCandy(candiesGiven[3]);
+			d.setCandy(candiesGiven[3]);
 			return d;
 		}
 		
 		return null;							// this can never happen, but Eclipse thinks we could get here, so...
-	}
-	
-	public StoryNode SkillCheck(Player p, int stat, int minRoll, StoryNode success, StoryNode fail)
-	{
-		int roll = (int) (Math.random() * 20 + 1);	//roll 1d20
-		if(stat == 0) roll += p.getCha();
-		if(stat == 1) roll += p.getDex();
-		if(stat == 2) roll += p.getStr();
-		
-		if(roll >= minRoll) return success;
-		return fail;
 	}
 	
 	public StoryNode(String str, StoryNode a, StoryNode b, StoryNode c, StoryNode d)
@@ -106,8 +105,10 @@ public class StoryNode
 		this.d = null;
 	}
 	
+	public void setCandy(Candy c) { toGive = c; }
 	public void setOutput(String out) { output = out; }
 	public void setCandy(Candy c, int n) { candiesGiven[n] = c; }
+	public void setNext(StoryNode n) { next = n; }
 	public void setA(StoryNode n) { a = n; }
 	public void setB(StoryNode n) { b = n; }
 	public void setC(StoryNode n) { c = n; }
@@ -116,12 +117,6 @@ public class StoryNode
 	public void setB(StoryNode n, String opt) { b = n; optB = opt;}
 	public void setC(StoryNode n, String opt) { c = n; optC = opt;}
 	public void setD(StoryNode n, String opt) { d = n; optD = opt;}
-	public void setSkillChecks(int[] checks) {
-		skillChecks[0] = checks[0];
-		skillChecks[1] = checks[1];
-		skillChecks[2] = checks[2];
-		skillChecks[3] = checks[3];
-	}
 	public void setEnding(int ending) { this.ending = ending; }
 	
 	public StoryNode getA() { return a; }
